3c7912e715f9fe7057c772176afbe2d5c5c630f1
Eliott Takvorian <98952935+eliotttak@users.noreply.github.com>
2025-02-19 17:19:41 +0100

    commit

--- serv-node-2.0.js
+++ serv-node-2.0.js
@@ -1,6 +1,41 @@
 const http = require("http")
 const fs = require("fs")
+const { verify } = require("crypto")
 const WebSocketServer = require("websocket").server
+const wsVersion = 2
+const httpPort = 8894
+
+const localIPv4 = (
+    function getIPAddress() {
+        var interfaces = require('os').networkInterfaces();
+        for (var devName in interfaces) {
+          var iface = interfaces[devName];
+      
+          for (var i = 0; i < iface.length; i++) {
+            var alias = iface[i];
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+              return alias.address;
+          }
+        }
+        return '0.0.0.0';
+      }
+)()
+
+const randomRange = (min, max) => Math.ceil(Math.random() * (max - min) + min)
+
+const randomArray = (...values) => values[randomRange(1, values.length) - 1]
+
+const randomString = (strLength) => {
+    let finalRandomString = ""
+    for (let x = 1 ; x <= strLength ; x++) {
+        possibleNumber = randomRange(0, 9)
+        possibleUpperCase = String.fromCharCode(randomRange(65, 90))
+        possibleLowerCase = String.fromCharCode(randomRange(97, 122))
+        finalRandomString += randomArray(possibleLowerCase, possibleNumber, possibleUpperCase)
+    }
+    return finalRandomString
+}
+
 const publicFiles = [
     "/",
     "/images/send-message-click.svg",
@@ -140,62 +175,88 @@ const server = http.createServer((request, result) => { // création du server
     })
 })
 
-server.listen(8894, "" , ()=>{
-    console.log("Bienvenue sur un chat en ligne à l'adresse 'http://192.168.1.25:8894' ( [ctrl] + clic gauche )\n")
+server.listen(httpPort, "" , ()=>{
+    console.log(`Bienvenue sur un chat en ligne à l'adresse 'http://${localIPv4}:${httpPort}' ( [ctrl] + clic gauche )\n`)
 })
 
 
 wsServer = new WebSocketServer({
     httpServer: server,
-    autoAcceptConnections: false
+    autoAcceptconnexions: false
+
 })
 
-let connections = []
-let pseudos = []
+let connexions = []
 
-wsServer.on("request", request => {
-    console.log("connection requested.\n")
-    const connection = request.accept(null, request.origin)
-    connections.push([connection, null]) // Ajouter la connexion et le pseudo (null pour le moment)
-    console.log("Connect accepted\n")
-    sendJsonMessage(connection, helloMsg)
+if (wsVersion === 1) { // The old version that needs to be reworked
 
-    connection.on("message", message => {
-        const valueObject = JSON.parse(message.utf8Data)
-        valueObject.value = valueObject.value.replaceAll("<", "&#60;").replaceAll("\n", "<br />")
-        if (valueObject.type === "requestPseudo") {
-            const index = connections.findIndex(([conn]) => conn === connection)
-            if (valueObject.value == "server" || pseudos.includes(valueObject.value.toLowerCase())) {
-                sendJsonMessage(connections[index][0], deniedPseudoMsg)
-                return
-            }
-            connections[index][1] = valueObject.value // Mettre à jour le pseudo
-            pseudos.push(valueObject.value.toLowerCase())
-            console.log(`Client ${valueObject.value} connected.\n`)
-            for (const [conn, pseudo] of connections) {
-                if (pseudo !== null) {
-                    sendJsonMessage(conn, { type: "message", value: `${valueObject.value} vient de se connecter.`, from: "server" });
+    wsServer.on("request", request => {
+        console.log("connection requested.\n")
+        console.log(request.origin)
+        const connection = request.accept(null, request.origin)
+        connexions.push([connection, null]) // Ajouter la connexion et le pseudo (null pour le moment)
+        console.log("Connect accepted\n")
+        sendJsonMessage(connection, helloMsg)
+    
+        connection.on("message", message => {
+            const valueObject = JSON.parse(message.utf8Data)
+            valueObject.value = valueObject.value.replaceAll("<", "&#60;").replaceAll("\n", "<br />")
+            if (valueObject.type === "requestPseudo") {
+                const index = connexions.findIndex(([conn]) => conn === connection)
+                if (valueObject.value == "server" || pseudos.includes(valueObject.value.toLowerCase())) {
+                    sendJsonMessage(connexions[index][0], deniedPseudoMsg)
+                    return
+                }
+                connexions[index][1] = valueObject.value // Mettre à jour le pseudo
+                pseudos.push(valueObject.value.toLowerCase())
+                console.log(`Client ${valueObject.value} connected.\n`)
+                for (const [conn, pseudo] of connexions) {
+                    if (pseudo !== null) {
+                        sendJsonMessage(conn, { type: "message", value: `${valueObject.value} vient de se connecter.`, from: "server" });
+                    }
+                }
+            } else {
+                for (const [conn, pseudo] of connexions) {
+                    if (pseudo !== null) {
+                        console.log({ ...valueObject, from: pseudo} + "\n")
+                        sendJsonMessage(conn, valueObject)
+                    }
                 }
             }
-        } else {
-            for (const [conn, pseudo] of connections) {
+        })
+    
+        connection.on("close", () => {
+            const index = connexions.findIndex(([conn]) => conn === connection)
+            const pseudo = connexions[index][1]
+            connexions.splice(index, 1) // Supprimer la connexion de la liste
+            console.log(`Client ${pseudo} disconnected.\n`)
+            for (const [conn, pseudo] of connexions) {
                 if (pseudo !== null) {
-                    console.log({ ...valueObject, from: pseudo} + "\n")
-                    sendJsonMessage(conn, valueObject)
+                    sendJsonMessage(conn, { type: "message", value: `${pseudo} vient de se déconnecter.`, from: "server" });
                 }
             }
-        }
+        })
     })
+}
 
-    connection.on("close", () => {
-        const index = connections.findIndex(([conn]) => conn === connection)
-        const pseudo = connections[index][1]
-        connections.splice(index, 1) // Supprimer la connexion de la liste
-        console.log(`Client ${pseudo} disconnected.\n`)
-        for (const [conn, pseudo] of connections) {
-            if (pseudo !== null) {
-                sendJsonMessage(conn, { type: "message", value: `${pseudo} vient de se déconnecter.`, from: "server" });
-            }
+else if (wsVersion === 2) { // The new version which is equivalent to the old one, but reworked.
+
+    wsServer.on("request", request => {
+        console.log(`A connexion was requested by a ${request.origin}'s client.\nWe are verifying that this is a correct URL (the correct URLs are 'http://localhost:${httpPort}' and 'http://${localIPv4}:${httpPort}')...\n`)
+        if (! new RegExp("https?:\/\/(localhost|" + localIPv4.replaceAll(".", "\.") + "):" + httpPort).test(request.origin)) {
+            console.log("The URL is not correct. The request is rejected.\n")
+            request.reject()
+            return
+        }
+        else {
+            console.log("The URL is correct. The request is accepted.\n")
+            connexions.push(
+                {
+                    wsConnexion: request.accept(),
+                    pseudo: undefined,
+                    verificationCode: randomString(12)
+                }
+            )
         }
     })
-})
+}
