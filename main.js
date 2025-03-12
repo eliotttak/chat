2f510d6eb58a02290c9b8fd6b61c13fda30be554
Eliott Takvorian <98952935+eliotttak@users.noreply.github.com>
2025-03-12 18:23:59 +0100

    Commit

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ let idOkpseudo = $("#okpseudo") // The 'Rejoindre le chat' (Join chat) button of
 let idInputpseudo = $("#inputpseudo") // The text input of the first page, where the user enters his/her pseudo.
 let pseudo // The user's pseudo
 let page = 0 // The page number : 0 is the first page where the user must input his/her pseudo, and 1 is the messages page.
-
+let dontSendUnconnectNotifs = false
+let inputError
 async function pause(time) {
     return new Promise(resolve => setTimeout(resolve, time))
 }
@@ -27,20 +28,22 @@ border-radius: ${options.bordrad || "5px"}
 }
 
 function putErrorUnderTextInput(input, txt = "Veuillez respecter le format", color = "#FF0000") {
+    debugger
     const width = input.width()
     const top = input.offset().top
     const height = input.height()
     const left = input.offset().left
 
-    let newSpan = document.createElement("span")
-    newSpan.style.position = "absolute"
-    newSpan.style.top = (top + height + 5) + "px"
-    newSpan.style.left = (left + 5) + "px"
-    newSpan.style.width = width + "px"
-    newSpan.style.color = color
-    newSpan.style.fontSize = "10px"
-    newSpan.innerHTML = txt
-    input.parent().append(newSpan)
+    input.parent().append('<span id="error-under-input"></span>')
+    let newSpan = $("#error-under-input")
+    newSpan.attr("id", "")
+    newSpan.css("position", "absolute")
+    newSpan.css("top", (top + height + 5) + "px")
+    newSpan.css("left", (left + 5) + "px")
+    newSpan.css("width", width + "px")
+    newSpan.css("color", color)
+    newSpan.css("font-size", "15px")
+    newSpan.html(txt)
 
-    console.log(width, height, top, left)
+    return newSpan
 }   
